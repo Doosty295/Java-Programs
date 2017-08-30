c2dab819c08e462db41196c94c09a7eb083aa4d2
Dustin LeGars <dlegars1976@gmail.com>
2017-08-30 10:14:18 -0400

    Java Project #2

---
+++ InitialD/src/initiald/InitialD.java
@@ -0,0 +1,30 @@
+/* Written In Atom
+  Compiled in NetBeansIDE
+  Written By LeGars, Dustin
+  Link to GitHub (https://github.com/Doosty295/Java-Programs)
+ */
+package initiald;
+
+/**
+ *
+ * @author 9djl001
+ */
+public class InitialD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("******         **********     **      ");
+        System.out.println("**    **            **        **      ");
+        System.out.println("**     **           **        **      ");
+        System.out.println("**      **          **        **      ");
+        System.out.println("**      **          **        **      ");
+        System.out.println("**      **          **        **      ");
+        System.out.println("**     **           **        **      ");
+        System.out.println("**    **       **   **        ********");
+        System.out.println("******           ****         ********");
+        
+    }
+    
+}
