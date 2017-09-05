bc480b51ff6015e6c3721c69f600d3aa25a28aaa
Dustin LeGars <dlegars1976@gmail.com>
2017-09-05 10:02:59 -0400

    Java Project 3

---
+++ Escape/src/escape/Escape.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package escape;
+
+/**
+ *
+ * @author 9djl001
+ */
+public class Escape {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("123456789012345678901234567890");
+        System.out.println("Hello World!"); 
+
+    }
+    
+}
