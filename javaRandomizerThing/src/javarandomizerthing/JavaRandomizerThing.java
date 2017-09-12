ca7d35de8c8a4bd7d8597c05499cdb30e18319c5
Doosty295 <31444117+Doosty295@users.noreply.github.com>
2017-09-12 06:41:35 -0400

    Just a Rnadom project
    Thought this would be fun. Was mistaken.

---
+++ javaRandomizerThing/src/javarandomizerthing/JavaRandomizerThing.java
@@ -0,0 +1,90 @@
+/*
+Compiled in NetBeans IDE 8.2
+Written in Atom
+Written by: Dustin LeGars
+Written on 09/11/2017
+---==+==---
+This is just a spur of the moment program that I decided to write. For fun. Send help.
+*/
+package javarandomizerthing;
+
+
+public class JavaRandomizerThing {
+  //Randomizer values
+    double aRand, bRand, cRand, dRand, eRand, fRand, gRand, hRand, iRand;
+  //Randomizers for aforementioned values
+      aRand = Math.random();
+      bRand = Math.random();
+      cRand = Math.random();
+      dRand = Math.random();
+      eRand = Math.random();
+      fRand = Math.random();
+      gRand = Math.random();
+      hRand = Math.random();
+
+      //For picking the randomizer combo.
+      iRand = Math.random();
+
+  //Averaging
+    double abRand, cdRand, efRand, ghRand;
+
+      //String randomization 1
+      abRand = ((aRand + bRand)/2);
+
+      //String randomization 2
+      cdRand = ((cRand + dRand)/2);
+
+      //String randomization 3
+      efRand = ((eRand + fRand)/2);
+
+      //String randomization 4
+      ghRand = ((gRand + hRand)/2);
+
+
+public static void main(String[] args) {
+
+  
+
+
+
+
+
+
+    }
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
