e73af3720a5ec726297b7aab1cb09f77d9f017d3
Dustin LeGars <dlegars1976@gmail.com>
2017-09-12 10:28:01 -0400

    Java Program 5

---
+++ indexOfProgram/src/indexofprogram/IndexOfProgram.java
@@ -0,0 +1,22 @@
+/*
+Written in Atom
+Compiled with NetBeans IDE 8.2
+Java Program 5 : Java Programming Period 3
+09/12/2017 - 10:05
+Dustin LeGars - 9djl001
+
+*/
+package indexofprogram;
+
+/**
+ *
+ * @author 9djl001
+ */
+public class IndexOfProgram {
+
+    public static void main(String[] args) {
+      String name = "Dustin LeGars";
+        System.out.println("First Name: " + name.substring(0,6) + "\nLast Name: " + name.substring(7));
+    }
+
+}
