422de4bbb1245977fb4e709d0abdb95f5024f5a0
Dustin LeGars <dlegars1976@gmail.com>
2017-09-14 10:01:43 -0400

    Java Program 4.2

---
+++ TriagonalSign/src/triagonalsign/TriagonalSign.java
@@ -0,0 +1,25 @@
+/*
+ Remake of program Triangle with Inputs
+ Written in Atom
+ Compiled in NetBeans IDE 8.2
+Written by Dustin LeGars on 9/14/2017
+Java Programming Period 3
+Java Program 4 v.2
+ */
+package triagonalsign;
+
+import java.util.Scanner;
+/**
+ *
+ * @author 9djl001
+ */
+public class TriagonalSign {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
