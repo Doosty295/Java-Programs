1047b437cffe2a94fceb982035d8acaa73400943
Dustin LeGars <dlegars1976@gmail.com>
2017-09-15 10:21:16 -0400

    Java Program 4.2

--- TriagonalSign/src/triagonalsign/TriagonalSign.java
+++ TriagonalSign/src/triagonalsign/TriagonalSign.java
@@ -14,11 +14,49 @@ import java.util.Scanner;
  * @author 9djl001
  */
 public class TriagonalSign {
+  //Declarations
+    double base, height, hyp, area, perim;
+    char queryInput;
+    Scanner inputBase, inputHeight, inputHyp, operationSelector = new Scanner(System.in);
 
-    /**
-     * @param args the command line arguments
-     */
+
+
     public static void main(String[] args) {
+
+//Operation query
+  System.out.println("What form of operation would you like to perform? \nTo caclulate for base, press \'A\'. \nTo calculate for height, press \'B\'. \nTo calculate for hypotenuse, press \'C\'. \nTo calculate for area, press \'D\'. \nTo calculate for perimeter, press \'E\'. \n\nTo terminate this program, press \'F\'.");
+  queryInput = input.nextChar();
+
+
+
+  //Switch
+    switch (queryInput) {
+    //Base
+      case 'A': System.out.println("What is the height?");
+        inputHeight = input.nextDouble();
+        System.out.println("What is the Hypotenuse?");
+        inputHyp = input.nextDouble();
+        
+    //Height
+      case 'B':
+
+    //Hypotenuse
+      case 'C':
+
+    //Area
+      case 'D':
+
+    //Perimeter
+      case 'E':
+
+    //Termination
+      case 'F':
+
+    }
+
+
+
+
 
     }
 
