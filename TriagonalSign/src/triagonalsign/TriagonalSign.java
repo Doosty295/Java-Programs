535c9cefb1342d541ad4e0af8239ba43ce09d3c6
Dustin LeGars <dlegars1976@gmail.com>
2017-09-15 15:04:40 -0400

    Java Program 4.2

--- TriagonalSign/src/triagonalsign/TriagonalSign.java
+++ TriagonalSign/src/triagonalsign/TriagonalSign.java
@@ -8,7 +8,8 @@ Java Program 4 v.2
  */
 package triagonalsign;
 
-import java.util.Scanner, java.util.Math;
+import java.util.Scanner;
+import Math.java;
 /**
  *
  * @author 9djl001
@@ -21,31 +22,24 @@ public class TriagonalSign {
 
 //Arithmetic
   //Base from Height and hyp
-    base = Math.sqrt((Math.pow(inputHyp, 2)) + (Math.pow(inputHeight, 2)));
+    double base = Math.sqrt((Math.pow(inputHyp, 2)) + (Math.pow(inputHeight, 2)));
 
   //Height from Base and hyp
-    height = Math.sqrt((Math.pow(inputHyp, 2)) + (Math.pow(inputBase, 2)));
+    double height = Math.sqrt((Math.pow(inputHyp, 2)) + (Math.pow(inputBase, 2)));
 
   //Hyp from Base and Height
-    hyp = Math.sqrt((Math.pow(inputBase, 2)) + (Math.pow(inputHeight, 2)));
+    double hyp = Math.sqrt((Math.pow(inputBase, 2)) + (Math.pow(inputHeight, 2)));
 
   //Area from Height and base
-    area = ((base / 2) * height);
+    double area = ((base / 2) * height);
 
   //Perimeter from Base, Height, and Hypotenuse
-    perim = (base + height + hyp);
+    double perim = (base + height + hyp);
 
     public static void main(String[] args) {
 
 //Operation query
-  System.out.println("What form of decimal based right triangle operation would you like to perform?
-  \nTo caclulate for base, press \'A\'.
-  \nTo calculate for height, press \'B\'.
-  \nTo calculate for hypotenuse, press \'C\'.
-  \nTo calculate for area, press \'D\'.
-  \nTo calculate for perimeter, press \'E\'.
-
-  \n\nTo terminate this program, press \'F\'.");
+  System.out.println("What form of decimal based right triangle operation would you like to perform? \nTo caclulate for base, press \'A\'. \nTo calculate for height, press \'B\'. \nTo calculate for hypotenuse, press \'C\'. \nTo calculate for area, press \'D\'. \nTo calculate for perimeter, press \'E\'. \n\nTo terminate this program, press \'F\'.");
 
   queryInput = input.nextChar();
 
@@ -100,11 +94,11 @@ public class TriagonalSign {
 
     //Termination
       case 'F':
-        System.end(0)
+        System.end(0);
       break;
 
     //Default
-    case default:
+    default :
       System.out.println("Please select an option to continue.");
       break;
 
