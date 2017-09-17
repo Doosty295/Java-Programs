0666cc233e59478cab087c339a7a70c413ac09c6
Doosty295 <31444117+Doosty295@users.noreply.github.com>
2017-09-17 12:21:59 -0400

    Java Program 4.2

--- TriagonalSign/src/triagonalsign/TriagonalSign.java
+++ TriagonalSign/src/triagonalsign/TriagonalSign.java
@@ -9,10 +9,6 @@ Java Program 4 v.2
 package triagonalsign;
 
 import java.util.Scanner;
-<<<<<<< HEAD
-=======
-import Math.java;
->>>>>>> 535c9cefb1342d541ad4e0af8239ba43ce09d3c6
 /**
  *
  * @author 9djl001
@@ -25,24 +21,23 @@ public class TriagonalSign {
 
 //Arithmetic
   //Base from Height and hyp
-    double base = Math.sqrt((Math.pow(inputHyp, 2)) + (Math.pow(inputHeight, 2)));
+     base = Math.sqrt((Math.pow(inputHyp, 2)) + (Math.pow(inputHeight, 2)));
 
   //Height from Base and hyp
-    double height = Math.sqrt((Math.pow(inputHyp, 2)) + (Math.pow(inputBase, 2)));
+     height = Math.sqrt((Math.pow(inputHyp, 2)) + (Math.pow(inputBase, 2)));
 
   //Hyp from Base and Height
-    double hyp = Math.sqrt((Math.pow(inputBase, 2)) + (Math.pow(inputHeight, 2)));
+     hyp = Math.sqrt((Math.pow(inputBase, 2)) + (Math.pow(inputHeight, 2)));
 
   //Area from Height and base
-    double area = ((base / 2) * height);
+     area = ((base / 2) * height);
 
   //Perimeter from Base, Height, and Hypotenuse
-    double perim = (base + height + hyp);
+     perim = (base + height + hyp);
 
     public static void main(String[] args) {
 
 //Operation query
-<<<<<<< HEAD
   System.out.println("What form of decimal based right triangle operation would you like to perform?");
    System.out.println("To caclulate for base, press 0.");
    System.out.println("To calculate for height, press 1.");
@@ -53,11 +48,6 @@ public class TriagonalSign {
 
    int ask;
         ask = input.nextInt();
-=======
-  System.out.println("What form of decimal based right triangle operation would you like to perform? \nTo caclulate for base, press \'A\'. \nTo calculate for height, press \'B\'. \nTo calculate for hypotenuse, press \'C\'. \nTo calculate for area, press \'D\'. \nTo calculate for perimeter, press \'E\'. \n\nTo terminate this program, press \'F\'.");
-
-  queryInput = input.nextChar();
->>>>>>> 535c9cefb1342d541ad4e0af8239ba43ce09d3c6
 
 
 
@@ -109,11 +99,7 @@ public class TriagonalSign {
       break;
 
     //Termination
-<<<<<<< HEAD
       case 5:
-=======
-      case 'F':
->>>>>>> 535c9cefb1342d541ad4e0af8239ba43ce09d3c6
         System.end(0);
       break;
 
