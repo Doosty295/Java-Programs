97b626e9fcbc1c87e5b5e1da10fb6bb764d56d24
Dustin LeGars <dlegars1976@gmail.com>
2017-09-20 10:07:24 -0400

    Java Program 6

--- InputStringAndStuff/src/inputstringandstuff/InputStringAndStuff.java
+++ InputStringAndStuff/src/inputstringandstuff/InputStringAndStuff.java
@@ -5,6 +5,7 @@ Written By Dustin LeGars
 Written on 9/18/2017 @ 10:00
 Recitation Assignment: User Input and String Methods
 Java Programming Period 3
+Java Program 6
  */
 package inputstringandstuff;
   import java.util.Scanner;
@@ -25,19 +26,24 @@ public class InputStringAndStuff {
       //Scanner
       Scanner spaceDust = new Scanner(System.in);
 
-
+      //Name
       String name;
-      System.out.println("Enter your name: " + name);
+      System.out.print("Enter your name: ");
       name = spaceDust.nextLine();
 
+      //Age
       int age;
-      System.out.println("Enter your current age: " + age);
+      System.out.print("Enter your current age: ");
       age = spaceDust.nextInt();
 
+      //Increase
       int years;
-      System.out.println("Enter the amount of years to increase: " + years);
-      years = paceDust.nextInt();
+      System.out.print("Enter the amount of years to increase: ");
+      years = spaceDust.nextInt();
 
+      //Last thing
+      System.out.println(name + ", your name is " + name.length() + " characters long and begins with the letter " + name.charAt(0) + ".");
+      System.out.println("In " + years + " you will be " + (years + age) + " years old and someone half your age would be " + (age / 2) + " years old.");
 
 
     }
