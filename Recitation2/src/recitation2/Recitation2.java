efb8d01cd40b99342db6cd611eb51f36278533e1
Dustin LeGars <dlegars1976@gmail.com>
2017-10-03 10:21:06 -0400

    Java Program 8 (I think)

---
+++ Recitation2/src/recitation2/Recitation2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+package recitation2;
+
+/**
+ *
+ * @author 9djl001
+ */
+public class Recitation2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Declarations
+        int length, space;
+        String name, extraS;
+        char initial1, initial2;
+
+        //Scanner
+        Scanner input = new Scanner;
+
+
+
+        //Interaction
+        System.out.println("Welcome to Recitation Project #1: Fun With Names. \nPlease input your name below.");
+        name = input.nextLine();
+        name = input.nextLine();
+
+        space = name.indexOf(' ');
+        initial1 = name.charAt(0);
+        initial2 = name.charAt(space + 1);
+
+
+        System.out.println("\n\n\nThank you for your name. \nYour name is " + name);
+
+        System.out.println("Your initials are: " + initial1 + initial2);
+
+
+
+    }
+
+}
