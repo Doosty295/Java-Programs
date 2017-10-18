d3422fa40d46a680b334c3c0209643a89ec33d93
Dustin LeGars <dlegars1976@gmail.com>
2017-10-18 09:59:14 -0400

    Java Program 9

--- DeciscionStructures/src/deciscionstructures/DeciscionStructures.java
+++ DeciscionStructures/src/deciscionstructures/DeciscionStructures.java
@@ -47,7 +47,7 @@ public class DeciscionStructures {
             System.out.println("Please input the name of the first runner.");
               runnerA = input.nextLine();
           //Time
-            System.out.println("Please input the time the first runner took.");
+            System.out.println("Please input the time the first runner took. As a whole number or a decimal.");
               timeA = input.nextDouble();
 
         //Section for Runner B
@@ -56,7 +56,7 @@ public class DeciscionStructures {
               runnerB = input.nextLine();
               runnerB = input.nextLine();
           //Time
-            System.out.println("Please input the time the second runner took.");
+            System.out.println("Please input the time the second runner took. As a whole number or a decimal.");
               timeB = input.nextDouble();
 
         //Section for Runner C
@@ -65,34 +65,54 @@ public class DeciscionStructures {
               runnerC = input.nextLine();
               runnerC = input.nextLine();
           //Time
-            System.out.println("Please input the time the third runner took.");
+            System.out.println("Please input the time the third runner took. As a whole number or a decimal.");
               timeC = input.nextDouble();
 
         //Placments
-          //A first, B second, C third
-      if (timeA < timeB && timeA < timeC && timeB < timeC) {
-        System.out.println("First place goes to " + runnerA + " with second place going to " + runnerB + " and finishing last is " + runnerC);
-
-          //A first, C second, B third
-      if (timeA < timeB && timeA < timeC && timeC < timeB) {
-        System.out.println("First place goes to " + runnerA + " with second place going to " + runnerC + " and finishing last is " + runnerB);
-
-          //B first, A second, C third
-      if (timeB < timeA && timeB < timeC && timeA < timeC) {
-        System.out.println("First place goes to " + runnerB + " with second place going to " + runnerA + " and finishing last is " + runnerC);
-
-          //B first, C second, A third
-      if (timeB < timeA && timeB < timeC && timeA < timeB) {
-        System.out.println("First place goes to " + runnerB + " with second place going to " + runnerC + " and finishing last is " + runnerA);
-
-          //C first, A second, B third
-      if (timeC < timeA && timeB < timeC && timeA < timeB) {
-        System.out.println("First place goes to " + runnerC + " with second place going to " + runnerA + " and finishing last is " + runnerB);
-
-          //C first, B second, A third
-      if (timeC < timeA && timeB < timeC && timeA < timeB) {
-        System.out.println("First place goes to " + runnerC + " with second place going to " + runnerB + " and finishing last is " + runnerA);
-
+          //A, B, C
+          if (timeA < timeB && timeB < timeC) {
+            System.out.println("The race is over! First place goes to " + runnerA + " followed by " + runnerB + " in second, and lastly, " + runnerC + " coming in last place.");
+          }
+          //A, C, B
+          if (timeA < timeC && timeC < timeB) {
+            System.out.println("The race is over! First place goes to " + runnerA + " followed by " + runnerC + " in second, and lastly, " + runnerB + " coming in last place.");
+          }
+          //B, A, C
+          if (timeB < timeA && timeA < timeC) {
+            System.out.println("The race is over! First place goes to " + runnerB + " followed by " + runnerA + " in second, and lastly, " + runnerC + " coming in last place.");
+          }
+          //B, C, A
+          if (timeB < timeC && timeC < timeA) {
+            System.out.println("The race is over! First place goes to " + runnerB + " followed by " + runnerC + " in second, and lastly, " + runnerA + " coming in last place.");
+          }
+          //C, A, B
+          if (timeC < timeA && timeA < timeB) {
+            System.out.println("The race is over! First place goes to " + runnerC + " followed by " + runnerA + " in second, and lastly, " + runnerB + " coming in last place.");
+          }
+          //C, B, A
+          if (timeC < timeB && timeB < timeA) {
+            System.out.println("The race is over! First place goes to " + runnerC + " followed by " + runnerB + " in second, and lastly, " + runnerA + " coming in last place.");
+          }
+          //Error
+          else {
+            double errCode = (Math.random() * 10);
+            if (errCode < 2) {
+              System.out.println("I just don't know what went wrong.");
+            }
+            else if (errCode < 4) {
+              System.out.println("Error: You broke it.");
+            }
+            else if (errCode < 6) {
+              System.out.println("There was a problem. It broke something. Probably something important.");
+            }
+            else if (errCode < 8) {
+              System.out.println("CATS: \'Someone set us up the error.\'");
+            }
+            else {
+              System.out.println("Error for the error gods!");
+            }
+            }
+          }
 
 
 
