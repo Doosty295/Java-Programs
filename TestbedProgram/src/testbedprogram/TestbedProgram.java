20f0c3734b796a9020432f5cb10d3ce23497cc10
Dustin LeGars <dlegars1976@gmail.com>
2017-11-08 10:27:41 -0500

    Testbed Program

---
+++ TestbedProgram/src/testbedprogram/TestbedProgram.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testbedprogram;
+
+/**
+ *
+ * @author 9djl001
+ */
+public class TestbedProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+
+     
+     
+    public static void main(String[] args) {
+            int i = 15;
+            short s = 25;
+            long m = 50;
+            float f = 2.5F;
+            double d = 0.25;
+            
+            m=i+1;
+            
+            System.out.println(i);
+            System.out.println(s);
+            System.out.println(m);
+            System.out.println(f);
+            System.out.println(d);
+        }
+    }
+    
+}
